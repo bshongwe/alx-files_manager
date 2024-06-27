d286b26637cfce282356da600959460144977701
bshongwe <dru.da.beat.hustle@gmail.com>
2024-06-27 13:43:51 +0200

    Task 2: Update 🚀✨ Set up Express server and API routes with status and stats endpoints 🌐🔧

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import injectRoutes from './routes';
+import startServer from './libs/boot';
+import injectMiddlewares from './libs/middlewares';
 
 const server = express();
 // const port = process.env.PORT || 5000;
@@ -8,6 +10,7 @@ const server = express();
 // app.use(express.json());
 injectMiddlewares(server);
 injectRoutes(server);
+startServer(server);
 
 // server.listen(port, () => {
 //	console.log(`Server is running on port ${port}`);
