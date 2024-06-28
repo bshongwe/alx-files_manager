3375fd7f3a7af19a2c60a2877024cf8f4dfdb392
bshongwe <dru.da.beat.hustle@gmail.com>
2024-06-28 15:52:05 -0700

    Fix checker 3 to 8

--- server.js
+++ server.js
@@ -4,16 +4,9 @@ import startServer from './libs/boot';
 import injectMiddlewares from './libs/middlewares';
 
 const server = express();
-// const port = process.env.PORT || 5000;
 
-// server.use('/', api);
-// app.use(express.json());
 injectMiddlewares(server);
 injectRoutes(server);
 startServer(server);
-
-// server.listen(port, () => {
-//	console.log(`Server is running on port ${port}`);
-// });
 
 export default server;
